f5814f4cead7b4aca6c573f83e8d1b445dd5705f
Cocoteax <97997374+Cocoteax@users.noreply.github.com>
2023-09-29 01:30:54 +0800

    Update BaseURL.js

--- Front-End/src/components/Utility/BaseURL.js
+++ Front-End/src/components/Utility/BaseURL.js
@@ -1,4 +1,4 @@
-// const BASE_URL = "https://e-commerce-app-rest-api.vercel.app";
-const BASE_URL = "http://localhost:5500";
+const BASE_URL = "https://e-commerce-app-rest-api.vercel.app";
+// const BASE_URL = "http://localhost:5500";
 
 export default BASE_URL;
